22c23e918464666f9e0a3448872b3d7a4c2df226
jj77847 <81172135+jj77847@users.noreply.github.com>
2022-04-27 21:29:55 +0100

    files and folders

--- src/componants/Todos.js
+++ src/componants/Todos.js
@@ -1,64 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 import { connect } from "react-redux";
-import { addTodos } from "../redux/reducer";
-
-const mapStateToProps = (state) => {
-  return {
-    todos: state,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    addTodo: (obj) => dispatch(addTodos(obj)),
-  };
-};
 
 const Todos = (props) => {
-  console.log("props", props);
-
   const [todo, setTodo] = useState("");
-
-  const add = () => {
-    if (todo === "") {
-      alert("Input is Empty");
-    } else {
-      props.addTodo({
-        id: Math.floor(Math.random() * 1000),
-        item: todo,
-        completed: false,
-      });
-      setTodo("");
-    }
-  };
 
   const handleChange = (e) => {
     setTodo(e.target.value);
   };
-
+  console.log("props from store", props);
   return (
     <div className="addTodos">
       <input
         type="text"
         onChange={(e) => handleChange(e)}
         className="todo-input"
-        value={todo}
       />
 
-      <button className="add-btn" onClick={() => add()}>
-        Add
-      </button>
-      <br />
-
-      <ul>
-        {props.todos.length > 0 &&
-          props.todos.map((item) => {
-            return <li key={item.id}>{item.item}</li>;
-          })}
-      </ul>
+      <button className="add-btn">ADD</button>
     </div>
   );
 };
 
-//we can use connect method to connect this component with redux store
-export default connect(mapStateToProps, mapDispatchToProps)(Todos);
+export default connect(null)(null)(Todos);
