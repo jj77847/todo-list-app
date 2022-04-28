8c3d73d4c5ec4c640dce6e4bd17065bbb0831249
jj77847 <81172135+jj77847@users.noreply.github.com>
2022-04-28 12:13:35 +0100

    wip

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const TodoItem = (props) => {
-    const {item, updateTodo, removeTodo, completeTodo} = props;
+  const { item, updateTodo, removeTodo, completeTodo } = props;
 
   const inputRef = useRef(true);
 
@@ -17,18 +17,19 @@ const TodoItem = (props) => {
     }
   };
 
-  return <li key={item.id}>
-              <textarea
-                ref={inputRef}
-                disabled={inputRef}
-                defaultValue={item.item}
-                onKeyPress={(e) => update(item.id, inputRef.current.value, e)}
-              />
-              <button onClick={() => changeFocus()}>Edit </button>
-              <button onClick={() => completeTodo(item.id)}>
-                Complete
-              </button>
-              <button onClick={() => removeTodo(item.id)}>Delete</button>{" "}
-            </li>
+  return (
+    <li key={item.id}>
+      <textarea
+        ref={inputRef}
+        disabled={inputRef}
+        defaultValue={item.item}
+        onKeyPress={(e) => update(item.id, inputRef.current.value, e)}
+      />
+      <button onClick={() => changeFocus()}>Edit </button>
+      <button onClick={() => completeTodo(item.id)}>Complete</button>
+      <button onClick={() => removeTodo(item.id)}>Delete</button>{" "}
+    </li>
+  );
+};
 
 export default TodoItem;
